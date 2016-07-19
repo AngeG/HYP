78cc9c53b971ed74ee9024422b26265e38b21289
AngeG <angegerace@gmail.com>
2016-07-19 22:23:24 +0200

    refresh

--- js/slcat.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $("#sl").on('click', function(e){
-        e.preventDefault();
-        alert("Pagine su questo SL non presenti. Guardare TIM MUSIC.");
-    });
-});
-
--- php/ajax_faq.php
+++
@@ -1,6 +0,0 @@
-<?php
-    if(isset($_POST['faq'])){
-        $header = file_get_contents('../html/musicfaq.html', FILE_USE_INCLUDE_PATH);
-        echo $header ;
-    }
-?>
